73160e2f0c795c9a03b699fd4060eb7114c51fd7
nbu-18cs <1154780761@qq.com>
2021-06-07 21:16:15 +0800

    jquery.min.js+pom.xml+Usercontroller

---
+++ src/main/java/tech/cncloud/chat_room/controller/UserController.java
@@ -0,0 +1,25 @@
+package tech.cncloud.chat_room.controller;
+
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.cncloud.chat_room.pojo.Result;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+public class UserController {
+    @RequestMapping("/login")
+    public Result login(User user, HttpSession session){
+        Result result=new Result();
+        if(user!=null && "123".equals(user.getPassword())){
+            result.setFlag(true);
+            session.setAttribute("user",user.getUsername());
+        }else{
+            result.setFlag(false);
+            result.setMessage("登录失败");
+        }
+
+        return result;
+    }
+}
