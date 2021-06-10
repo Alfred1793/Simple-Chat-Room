49e950d7280ae1362357b231067774ddf5d87233
Alfred1793 <43002626+Alfred1793@users.noreply.github.com>
2021-06-10 15:40:46 +0800

    update login

---
+++ src/main/java/tech/cncloud/chat_room/pojo/User.java
@@ -0,0 +1,24 @@
+package tech.cncloud.chat_room.pojo;
+
+import javax.swing.*;
+
+public class User {
+    String name;
+    String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
