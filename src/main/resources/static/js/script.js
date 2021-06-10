3c40f59ae31d601e9afa9a228c8a1958c9787ad3
Alfred1793 <43002626+Alfred1793@users.noreply.github.com>
2021-06-10 14:06:07 +0800

    fix some bugs again

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,10 +1,14 @@
 $(function(){
-    $("#bin").click(function (){
+    console.log("the script is running");
+    $("#btn").click(function (){
         $.get("login",$("#loginForm").serialize(),function(res){
+            console.log("get click!!!");
             if(res.flag){//如果登录成功，跳转
+                console.log("success!!!");
                 location.href="main.html";
             }else{//否则显示错误信息
-                $("#err_msg").html(res.message());
+                $("#err_msg").html(res.message);
+                console.log("fail");
             }
         },"json");
     });
